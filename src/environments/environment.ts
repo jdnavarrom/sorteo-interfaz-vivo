bdc9d6ce1064d5b2d5ff37556e28fdb091572d82
FallasDv <fallas@coopetrrazu.com>
2025-11-28 07:25:13 -0600

    config produccion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  endpoint: 'http://localhost:8081',
-  // endpoint: 'https://sorteos-live.coopetarrazu.dev',
+  // endpoint: 'http://localhost:8081',
+  endpoint: 'https://sorteos-live.coopetarrazu.dev',
 };
